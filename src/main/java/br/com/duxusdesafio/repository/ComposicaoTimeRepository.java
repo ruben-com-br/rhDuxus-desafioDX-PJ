1a192a7aef43a564c573d04676eeddce68231a61
Ruben Borges <ruben-rbr@overmind.ai>
2025-05-09 15:41:56 -0300

    feat: cria repositorys

--- src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
@@ -1,7 +1,12 @@
 package br.com.duxusdesafio.repository;
 
 import br.com.duxusdesafio.model.ComposicaoTime;
+import br.com.duxusdesafio.model.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ComposicaoTimeRepository extends JpaRepository<ComposicaoTime, Long> {
+    List<ComposicaoTime> findAllByTime(Time time);
 }
--- src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
@@ -3,5 +3,10 @@ package br.com.duxusdesafio.repository;
 import br.com.duxusdesafio.model.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface TimeRepository extends JpaRepository<Time, Long> {
+
+    Optional<Time> findByData(LocalDate data);
 }
