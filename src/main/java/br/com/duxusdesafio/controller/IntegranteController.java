ab3035008a6d6f7fc91312c7c7664b777da4e052
Ruben Borges <ruben-rbr@overmind.ai>
2025-05-09 07:56:28 -0300

    feat: cria endpoint para gravar Integrante, Controller e Service

--- src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
+++ src/main/java/br/com/duxusdesafio/controller/IntegranteController.java
@@ -1,8 +1,13 @@
 package br.com.duxusdesafio.controller;
 
+import br.com.duxusdesafio.dto.request.IntegranteRequestDTO;
 import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.repository.IntegranteRepository;
+import br.com.duxusdesafio.service.IntegranteService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -10,13 +15,22 @@ import java.util.List;
 public class IntegranteController {
 
     private final IntegranteRepository repository;
+    private final IntegranteService service;
 
-    public IntegranteController(IntegranteRepository repository) {
+    public IntegranteController(IntegranteRepository repository,
+                                IntegranteService service) {
         this.repository = repository;
+        this.service = service;
     }
 
     @GetMapping
     public List<Integrante> findAll() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Integrante> create(@RequestBody IntegranteRequestDTO dto) {
+        Integrante saved = service.salvar(dto);
+        return ResponseEntity.created(URI.create("/integrantes/" + saved.getId())).body(saved);
     }
 }
---
+++ src/main/java/br/com/duxusdesafio/service/IntegranteService.java
@@ -0,0 +1,24 @@
+package br.com.duxusdesafio.service;
+
+import br.com.duxusdesafio.dto.request.IntegranteRequestDTO;
+import br.com.duxusdesafio.model.Integrante;
+import br.com.duxusdesafio.repository.IntegranteRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IntegranteService {
+
+    private final IntegranteRepository repository;
+
+    public IntegranteService(IntegranteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Integrante salvar(IntegranteRequestDTO dto) {
+        Integrante integrante = new Integrante();
+        integrante.setFranquia(dto.getFranquia());
+        integrante.setNome(dto.getNome());
+        integrante.setFuncao(dto.getFuncao());
+        return repository.save(integrante);
+    }
+}
